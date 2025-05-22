f16ded0e8de0c198c7c3da5c9a152be31078d9e9
anyashliakova2002 <75955858+anyashliakova2002@users.noreply.github.com>
2025-05-23 02:03:12 +0300

    Update httpd.h

--- 04.pico-foxweb/httpd.h
+++ 04.pico-foxweb/httpd.h
@@ -4,16 +4,28 @@
 #include <stdio.h>
 #include <string.h>
 #include <time.h>
+#include <security/pam_appl.h>
+#include <openssl/ssl.h>
+#include <openssl/err.h>
 
 // Client request
-extern char *method, // "GET" or "POST"
-    *uri,            // "/index.html" things before '?'
-    *qs,             // "a=1&b=2" things after  '?'
-    *prot,           // "HTTP/1.1"
-    *payload;        // for POST
-
+extern char *method, *uri, *qs, *prot, *payload;
 extern int payload_size;
 extern char *client_ip;
+extern SSL_CTX *ssl_ctx;
+
+// PAM authentication
+typedef struct {
+    char *username;
+    char *password;
+} pam_conv_data;
+
+int pam_authenticate_user(const char *username, const char *password);
+void init_openssl();
+SSL_CTX *create_context();
+void configure_context(SSL_CTX *ctx, const char *cert_file, const char *key_file);
+void cleanup_openssl();
+char *base64_decode(const char *src);
 
 // Server control functions
 void serve_forever(const char *PORT);
@@ -22,12 +34,11 @@ void log_access(const char *status, int response_size);
 char *request_header(const char *name);
 
 typedef struct {
-  char *name, *value;
+    char *name, *value;
 } header_t;
 static header_t reqhdr[17] = {{"\0", "\0"}};
 header_t *request_headers(void);
 
-// user shall implement this function
 void route();
 
 // Response
@@ -35,18 +46,15 @@ void route();
 
 #define HTTP_200 printf("%s 200 OK\n\n", RESPONSE_PROTOCOL)
 #define HTTP_201 printf("%s 201 Created\n\n", RESPONSE_PROTOCOL)
+#define HTTP_401 printf("%s 401 Unauthorized\nWWW-Authenticate: Basic realm=\"Secure Area\"\n\n", RESPONSE_PROTOCOL)
 #define HTTP_404 printf("%s 404 Not found\n\n", RESPONSE_PROTOCOL)
 #define HTTP_500 printf("%s 500 Internal Server Error\n\n", RESPONSE_PROTOCOL)
 
-// some interesting macro for `route()`
+// Routing macros
 #define ROUTE_START() if (0) {
-#define ROUTE(METHOD, URI)                                                     \
-  }                                                                            \
-  else if (strcmp(URI, uri) == 0 && strcmp(METHOD, method) == 0) {
+#define ROUTE(METHOD, URI) } else if (strcmp(URI, uri) == 0 && strcmp(METHOD, method) == 0) {
 #define GET(URI) ROUTE("GET", URI)
 #define POST(URI) ROUTE("POST", URI)
-#define ROUTE_END()                                                            \
-  }                                                                            \
-  else HTTP_500;
+#define ROUTE_END() } else HTTP_500;
 
 #endif
